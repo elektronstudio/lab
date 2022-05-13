9c826aad41c5ac8f97f698980e93c6a6da8a9cfc
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-05-13 10:44:30 +0300

    cleanup

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -4,19 +4,9 @@ import { line } from "d3-shape";
 import { scaleTime, scaleLinear } from "d3-scale";
 import { extent } from "d3-array";
 import { add } from "date-fns";
+import { polygonpath } from "fachwerk";
 
-const props = defineProps(["data", "timestamp", "start", "sel"]);
-watchEffect(() => console.log(props.sel));
-
-//const data = [];
-// const data = [
-//   { value: -2, userId: "a12", datetime: add(new Date(), { seconds: 1 }) },
-//   { value: 421, userId: "a12", datetime: add(new Date(), { seconds: 10 }) },
-//   { value: 421, userId: "a12", datetime: add(new Date(), { seconds: 40 }) },
-//   { value: 1000, userId: "ffa", datetime: add(new Date(), { seconds: 1 }) },
-//   { value: 212, userId: "ffa", datetime: add(new Date(), { seconds: 10 }) },
-//   { value: 21, userId: "ffa", datetime: add(new Date(), { seconds: 40 }) },
-// ];
+const props = defineProps(["data", "timestamp", "start", "sel", "data2"]);
 
 const width = 800;
 const height = 100;
@@ -34,6 +24,11 @@ const y = (dd) =>
   scaleLinear()
     .domain(extent(dd, (d) => d.value.length))
     .range([p, height - p]);
+
+const y2 = (dd) =>
+  scaleLinear()
+    .domain(extent(dd, (d) => parseFloat(d.value)))
+    .range([p, height * 0.8 - p]);
 
 // const y = computed(() =>
 //   scaleLinear()
@@ -68,6 +63,31 @@ const y = (dd) =>
       opacity="0.5"
       fill="none"
     />
+    <!-- <line
+      v-for="(c, i) in data2"
+      :x1="x(data)(new Date(c.datetime))"
+      :y1="height - parseFloat(c.value) * 8"
+      :x2="x(data)(new Date(c.datetime))"
+      :y2="height"
+      stroke="yellow"
+      stroke-width="3"
+      opacity="1"
+      fill="none"
+    /> -->
+    <path
+      :d="
+        polygonpath(
+          data2.map((d) => [
+            x(data)(new Date(d.datetime)),
+            height - parseFloat(d.value) * 8,
+          ])
+        )
+      "
+      stroke="yellow"
+      stroke-width="2"
+      opacity="1"
+      fill="none"
+    />
     <line
       :x1="x(data)(new Date(timestamp))"
       :y1="0"
@@ -79,6 +99,4 @@ const y = (dd) =>
       fill="none"
     />
   </svg>
-  <!-- {{ new Date(timestamp) }} -->
-  <br />
 </template>
